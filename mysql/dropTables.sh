9a88cf687e21cf95008b7761aad247a5732654f2
Paul Chabot <paulchabot@Pauls-Macintosh.local>
2017-09-14 01:17:05 -0400

    must ask for password, before making actions on the db..

--- mysql/dropTables.sh
+++ mysql/dropTables.sh
@@ -4,7 +4,7 @@
 # Original script from https://www.cyberciti.biz/faq/how-do-i-empty-mysql-database/
 
 #!/bin/bash
-ER="$1"
+MUSER="$1"
 MDB="$2"
 
 # Detect paths
@@ -18,11 +18,12 @@ then
     echo "Drops all tables from a MySQL"
     exit 1
 fi
+# Ask for password, keeps it off the command line and out of the bash history file.
+echo -n Enter Password for $MUSER:
+read -s MPASS
 
 TABLES=$($MYSQL -u $MUSER -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
 
-echo -n Enter Password for $MUSER:
-read -s MPASS
 
 for t in $TABLES
 do
